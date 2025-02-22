f99b77bd79ab645461ee933ad8790157f50ec5b8
Bhakti Garach <garab299@newschool.edu>
2025-02-21 19:23:38 -0500

    updates

--- assets/arena.js
+++ assets/arena.js
@@ -1,15 +1,6 @@
 // Use botton
 // Use modal
 // Use filter
-// Mobile Menu Toggle
-let menuToggle = document.querySelector('#menu-toggle');
-let navLinks = document.querySelector('#nav-links');
-
-menuToggle.onclick = () => { 
-    navLinks.classList.toggle('active'); 
-    menuToggle.textContent = navLinks.classList.contains('active') ? '[CLOSE]' : '[MENU]'; 
-};
-
 
 // This allows us to process/render the descriptions, which are in Markdown!
 // More about Markdown: https://en.wikipedia.org/wiki/Markdown
@@ -27,15 +18,16 @@ let placeChannelInfo = (data) => {
 	// Target some elements in your HTML:
 	let channelTitle = document.querySelector('#channel-title')
 	let channelDescription = document.querySelector('#channel-description')
-	let channelCount = document.querySelector('#channel-count')
+	// let channelCount = document.querySelector('#channel-count')
 	let channelLink = document.querySelector('#channel-link')
 
 	// Then set their content/attributes to our data:
 	channelTitle.innerHTML = data.title
 	channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
-	channelCount.innerHTML = data.length
+	// channelCount.innerHTML = data.length
 	channelLink.href = `https://www.are.na/channel/${channelSlug}`
 }
+
 
 
 // Then our big function for specific-block-type rendering:
@@ -47,109 +39,164 @@ let renderBlock = (block) => {
 	if (block.class == 'Link') {
 		let linkItem =
 			`
-			<div>
-				<p><em>Link</em></p>
-				<picture>
+			<li class="link-block">
+                <button>
+				    <p><em>Link</em></p>
+				    <picture>
 					<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">
 					<source media="(max-width: 640px)" srcset="${ block.image.large.url }">
 					<img src="${ block.image.original.url }">
-				</picture>
-				<h3>${ block.title }</h3>
-				${ block.description_html }
-				<p><a href="${ block.source.url }">See the original ↗</a></p>
-			</div>
+				    </picture>
+				    
+			
+				    <p><a href="${ block.source.url }">See the original ↗</a></p>
+                </button>
+                <dialog>
+					<div>
+						<p>${block.title}</p>
+						<p>${block.description_html}</p>
+					</div>
+					<button class="close">×</button>
+				</dialog>
+			</li>
 			`
 		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
 	}
 
-// Images!
-else if (block.class == 'Image') {
-    // …up to you!
-}
+	// Images!
+	else if (block.class == 'Image') {
+        let imageItem = `
+        <li class="image-block">
+            <button>
+                <figure>
+                    <image src="${block.image.large.url}">
+                    <figcaption>${block.title}</figcaption>
+                </figure>
+            </button>
+            <dialog>
+                <div>
+                    <p>${block.title}</p>
+                    <p>${block.description_html}</p>
+                </div>
+                <img src="${block.image.large.url}">
+                <button class="close">×</button>
+            </dialog>
+        </li>
+    `
+    channelBlocks.insertAdjacentHTML('beforeend', imageItem)
+	}
 
-// Text!
-else if (block.class == 'Text') {
-    // …up to you!
-}
+	// Text!
+	else if (block.class == 'Text') {
+		// …up to you!
+	}
 
-// Uploaded (not linked) media…
-else if (block.class == 'Attachment') {
-    let attachment = block.attachment.content_type // Save us some repetition
+	// Uploaded (not linked) media…
+	else if (block.class == 'Attachment') {
+		let attachment = block.attachment.content_type // Save us some repetition
 
-    // Uploaded videos!
-    if (attachment.includes('video')) {
-        // …still up to you, but we’ll give you the `video` element:
-        let videoItem =
-            `
-            <li>
-                <p><em>Video</em></p>
-                <video controls src="${ block.attachment.url }"></video>
-            </li>
-            `
-        channelBlocks.insertAdjacentHTML('beforeend', videoItem)
-        // More on video, like the `autoplay` attribute:
-        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
-    }
+		// Uploaded videos!
+		if (attachment.includes('video')) {
+			// …still up to you, but we’ll give you the `video` element:
+			let videoItem =
+				`
+				<li>
+					<p><em>Video</em></p>
+					<video controls src="${ block.attachment.url }">
+                    </video>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
+			// More on video, like the `autoplay` attribute:
+			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+		}
 
-    // Uploaded PDFs!
-    else if (attachment.includes('pdf')) {
-        // …up to you!
-    }
+		// Uploaded PDFs!
+		else if (attachment.includes('pdf')) {
+			// …up to you!
+		}
 
-    // Uploaded audio!
-    else if (attachment.includes('audio')) {
-        // …still up to you, but here’s an `audio` element:
-        let audioItem =
-            `
-            <li>
-                <p><em>Audio</em></p>
-                <audio controls src="${ block.attachment.url }"></video>
-            </li>
-            `
-        channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-        // More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
-    }
-}
+		// Uploaded audio!
+		else if (attachment.includes('audio')) {
+			// …still up to you, but here’s an `audio` element:
+			let audioItem =
+				`
+				<li>
+					<p><em>Audio</em></p>
+					<audio controls src="${ block.attachment.url }"></audio>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
+			// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+		}
+	}
 
-// Linked media…
-else if (block.class == 'Media') {
-    let embed = block.embed.type
+	// Linked media…
+	else if (block.class == 'Media') {
+		let embed = block.embed.type
 
-    // Linked video!
-    if (embed.includes('video')) {
-        // …still up to you, but here’s an example `iframe` element:
-        let linkedVideoItem =
-            `
-            <li>
-                <p><em>Linked Video</em></p>
-                ${ block.embed.html }
-            </li>
-            `
-        channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
-        // More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
-    }
+		// Linked video!
+		if (embed.includes('video')) {
+			// …still up to you, but here’s an example `iframe` element:
+			let linkedVideoItem =
+				`
+				<li>
+					<p><em>Linked Video</em></p>
+					${ block.embed.html }
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
+			// More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+		}
 
-    // Linked audio!
-    else if (embed.includes('rich')) {
-        // …up to you!
-    }
-}
+		// Linked audio!
+		else if (embed.includes('rich')) {
+			// …up to you!
+		}
+	}
 }
 
 
 
 // It‘s always good to credit your work:
 let renderUser = (user, container) => { // You can have multiple arguments for a function!
-let userAddress =
-    `
-    <address>
-        <img src="${ user.avatar_image.display }">
-        <h3>${ user.first_name }</h3>
-        <p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
-    </address>
-    `
-container.insertAdjacentHTML('beforeend', userAddress)
+	let userAddress =
+		`
+		<address>
+			<img src="${ user.avatar_image.display }">
+			<h3>${ user.first_name }</h3>
+			<p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
+		</address>
+		`
+	container.insertAdjacentHTML('beforeend', userAddress)
 }
+
+
+let initInteraction = () => {
+	let blocks = document.querySelectorAll('.image-block, .link-block')
+	blocks.forEach((block) => {
+		let openButton = block.querySelector('button')
+		let dialog = block.querySelector('dialog')
+		let closeButton = dialog.querySelector('button')
+
+		openButton.onclick = () => {
+			dialog.showModal()
+		}
+
+		closeButton.onclick = () => {
+			dialog.close()
+		}
+
+		dialog.onclick = (event) => { // Listen on our `modal` also…
+			if (event.target == dialog) { // Only if clicks are to itself (the background).
+				dialog.close() // Close it then too.
+			}
+		}
+	})
+}
+
+
+
 
 
 // Now that we have said what we can do, go get the data:
@@ -171,6 +218,8 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 		renderUser(data.user, channelUsers)
 	})
 
+    initInteraction()
+
 // Title Stretch on Scroll
 window.addEventListener("scroll", () => {
     let scrollY = window.scrollY;
@@ -179,3 +228,18 @@ window.addEventListener("scroll", () => {
 
     document.documentElement.style.setProperty('--svg-height', `${newHeight}vh`);
 });
+
+
+// Remove
+// document.querySelector('#channel-count').remove();
+
+
+// Mobile Menu Toggle
+let menuToggle = document.querySelector('#menu-toggle');
+let navLinks = document.querySelector('#nav-links');
+
+menuToggle.onclick = () => { 
+    navLinks.classList.toggle('active'); 
+    menuToggle.textContent = navLinks.classList.contains('active') ? '[CLOSE]' : '[MENU]'; 
+};
+
